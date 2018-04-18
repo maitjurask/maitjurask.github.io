b1339e9bce62e0276ed2b14fa02c525f19ced210
maitjurask <38127136+maitjurask@users.noreply.github.com>
2018-04-18 10:58:30 +0200

    Header background and z-index

--- js/navbar.js
+++ js/navbar.js
@@ -6,9 +6,11 @@ var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
 
 function myFunction() {
-	if (window.pageYOffset >= sticky) {
+	if (window.scrollY >= sticky) {
+		document.body.style.paddingTop = navbar.offsetHeight + 'px';
 		navbar.classList.add("sticky");
 	} else {
+		document.body.style.paddingTop = 0;
 		navbar.classList.remove("sticky");
 	}
 }
